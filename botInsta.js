5a5ebef3acb674e60479dcd2b2af41ae6dc4debf
ctrejos <camilotm89@gmail.com>
2021-09-29 23:03:12 -0500

    Create botInsta.js

---
+++ botInsta.js
@@ -0,0 +1,13 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+for(var i = 0; i<50; i++){
+        
+    like = document.getElementsByClassName("wpO6b"); 
+    next = document.getElementsByClassName("_65Bje");
+    like[1].click();
+    next[0].click();
+    await sleep(500);
+
+}
